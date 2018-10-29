030f7507d7c7ada301c20ade038cad79cf3a371f
Yainel García Alfonso <garcia.yainel@gmail.com>
2018-10-29 14:17:29 -0500

    Agredada linea para crear la base de datos

--- www/sql/believecuba.sql
+++ www/sql/believecuba.sql
@@ -17,6 +17,8 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
+CREATE DATABASE believecuba;
+
 -- ----------------------------
 -- Table structure for auto_categoria
 -- ----------------------------
