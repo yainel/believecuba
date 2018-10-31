280eae1cc7ef3bcf9271eefa03e3e4a3a6ac62a5
Yainel García Alfonso <garcia.yainel@gmail.com>
2018-10-31 01:12:33 -0500

    Corregido problema Autoload CKEditor

---
+++ www/src/AppBundle/Resources/Public/js/common-pages.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+//waves effect js start
+ Waves.init();
+    Waves.attach('.flat-buttons', ['waves-button']);
+    Waves.attach('.float-buttons', ['waves-button', 'waves-float']);
+    Waves.attach('.float-button-light', ['waves-button', 'waves-float', 'waves-light']);
+    Waves.attach('.flat-buttons',['waves-button', 'waves-float', 'waves-light','flat-buttons']);
+//waves effect js end
+
+});
---
+++ www/src/AppBundle/Resources/Public/pages/slider.js
@@ -0,0 +1,111 @@
+'use strict';
+$(document).ready(function() {
+    /*owl carosel slider */
+    $("#idslidercenter").owlCarousel({
+        dots: true,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:9000,
+        margin:0,
+        smartSpeed:1000,
+        loop:true,
+        slideSpeed:300,
+        items:4,
+        // rtl: true, this start a image from right side to left
+        singleItem:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:2,
+                nav:true
+            },
+            1000:{
+                items:4,
+                nav:true,
+                loop:false
+            }
+        }
+
+    });
+    $("#todaydeals").owlCarousel({
+        dots: false,
+        nav:true,
+        autoplayTimeout:9000,
+        margin:16,
+        smartSpeed:1000,
+        loop:false,
+        slideSpeed:300,
+        items:3,
+        // rtl: true, this start a image from right side to left
+        singleItem:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+                loop:false
+            }
+        }
+
+    });
+    $("#todaysdestinations").owlCarousel({
+        nav:true,
+        autoplayTimeout:9000,
+        margin:16,
+        smartSpeed:1000,
+        loop:false,
+        slideSpeed:300,
+        items:3,
+        // rtl: true, this start a image from right side to left
+        singleItem:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+                loop:false
+            }
+        }
+
+    });
+    $("#recomendedcars").owlCarousel({
+        nav:true,
+        autoplayTimeout:9000,
+        margin:16,
+        smartSpeed:1000,
+        loop:false,
+        slideSpeed:300,
+        items:4,
+        // rtl: true, this start a image from right side to left
+        singleItem:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:4,
+                loop:false
+            }
+        }
+
+    });
+
+});
